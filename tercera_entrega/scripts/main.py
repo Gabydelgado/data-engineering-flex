00651bdd152014f1216396648a65538ebb3f3b04
Gabydelgado <45246416+Gabydelgado@users.noreply.github.com>
2023-12-04 23:49:08 -0300

    Create main.py

---
+++ tercera_entrega/scripts/main.py
@@ -0,0 +1,32 @@
+import requests
+import pandas as pd
+#import utils
+from scripts import utils
+
+from configparser import ConfigParser
+
+def main(config):
+
+    parser = ConfigParser()
+    parser.read(config)
+    url = 'https://api.coincap.io/v2/rates'
+    response = requests.get(url)
+
+    if response.status_code == 200:
+
+        json_data = response.json()
+        #Transforma datos en formato JSON en un DataFrame de Pandas.
+        df = utils.tranforms_json(json_data)
+        #Crea la conexión a la base de datos
+        conn = utils.connect_db(parser)
+        schema=parser['DB']['schema']
+        #Carga datos en la base de datos desde un DataFrame.
+        utils.load_data(df, conn, schema)
+        print('Se cargaron los datos exitosamente')
+    else:
+        print('Error al llamar a la API')
+
+if __name__ == '__main__':
+    config = './config.ini'
+
+    main(config)
