16d85be445763fe60f86e7e577407de29696a963
Gabydelgado <45246416+Gabydelgado@users.noreply.github.com>
2023-12-21 16:47:39 -0300

    Update dag_etl.py

--- entrega_final/dags/dag_etl.py
+++ entrega_final/dags/dag_etl.py
@@ -2,27 +2,24 @@ from datetime import datetime, timedelta
 
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
-from airflow.operators.dummy import DummyOperator
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 
 from scripts.main import main
 
 default_args = {
+    'owner': 'Gabriel Delgado',
     'retries': 3,
     'retry_delay': timedelta(minutes=5)
     }
 
 with DAG(
-    dag_id = 'dag_etl',
+    dag_id = 'dag_crypto_currency',
+    description='DAG para consultar y almacenar valores de criptomonedas',
     start_date = datetime(2023, 12, 4),
     catchup = False,
     schedule_interval = '@daily',
     default_args = default_args
     ) as dag:
-
-    dummy_start = DummyOperator(
-        task_id = 'start'
-        )
 
     create_table = PostgresOperator(
         task_id = 'create_table',
@@ -36,9 +33,5 @@ with DAG(
         python_callable = main,
         op_kwargs = {'config': '/opt/airflow/config/config.ini'}
         )
-
-    dummy_end = DummyOperator(
-        task_id = 'end'
-        )
     
-    dummy_start >> create_table >> main_task >> dummy_end
+    create_table >> main_task
